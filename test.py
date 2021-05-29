79e0cc41fd36168def93b0f2036e07d79e1bcbe9
markser <serm@oregonstate.edu>
2021-05-28 20:28:50 -0700

    hopefully fixed lint errors

--- test.py
+++ test.py
@@ -1,56 +1,56 @@
 import unittest
 import calc
 
-# run with python3 command 
+# run with python3 command
+
 
 class TestCalculator(unittest.TestCase):
     def test_add_pass(self):
-        actual = calc.add(1,2)
+        actual = calc.add(1, 2)
         expected = 3
         self.assertEqual(actual, expected)
-    
+
     def test_add_fail(self):
-        actual = calc.add(1,2)
+        actual = calc.add(1, 2)
         expected = -1
         self.assertEqual(actual, expected)
 
     def test_sub_pass(self):
-        actual = calc.sub(1,2)
+        actual = calc.sub(1, 2)
         expected = -1
         self.assertEqual(actual, expected)
-    
+
     def test_sub_fail(self):
-        actual = calc.sub(1,2)
+        actual = calc.sub(1, 2)
         expected = 10
         self.assertEqual(actual, expected)
 
     def test_mul_pass(self):
-        actual = calc.mul(1,2)
+        actual = calc.mul(1, 2)
         expected = 2
         self.assertEqual(actual, expected)
-    
+
     def test_mul_fail(self):
-        actual = calc.mul(1,2)
+        actual = calc.mul(1, 2)
         expected = -1
         self.assertEqual(actual, expected)
 
     def test_div_pass(self):
-        actual = calc.div(1,2)
+        actual = calc.div(1, 2)
         expected = 0.5
         self.assertEqual(actual, expected)
-    
+
     def test_div_fail(self):
-        actual = calc.div(1,2)
+        actual = calc.div(1, 2)
         expected = 1
         self.assertEqual(actual, expected)
-    
 
     def test_invalid_input(self):
         with self.assertRaises(TypeError):
-            calc.add("a","b")
-            calc.sub("a","b")
-            calc.mul("a","b")
-            calc.div("a","b")
+            calc.add("a", "b")
+            calc.sub("a", "b")
+            calc.mul("a", "b")
+            calc.div("a", "b")
 
 
 if __name__ == '__main__':
